91c005af99619d010ae13c2dc4ea946abbeb4cda
RileyC11 <cor20046@byui.edu>
2022-05-04 13:44:05 -0600

    added week 3

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Hello world
 No hello world
 Hello again
+Uh oh 
---
+++ W03 Prove Hilo/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("");
+Console.WriteLine("Why must git be so confusing.");
+Console.WriteLine("I'm so happy!");

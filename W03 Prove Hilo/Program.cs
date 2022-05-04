bfd05317ac7f8916d857d7f30c3bbe30177aa023
RileyC11 <cor20046@byui.edu>
2022-05-04 13:56:55 -0600

    starting fresh

--- W03 Prove Hilo/Program.cs
+++ W03 Prove Hilo/Program.cs
@@ -1,6 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("");
-Console.WriteLine("Why must git be so confusing.");
-Console.WriteLine("I'm so happy!");
-Console.WriteLine("I'm so sad :(");
+﻿

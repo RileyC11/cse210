e5d603b6770a920b2279b5e7760d8863e3e08ecf
RileyC11 <cor20046@byui.edu>
2022-05-09 18:05:07 -0600

    working code

--- W03_Prove_Hilo_Game/Game/Card.cs
+++ W03_Prove_Hilo_Game/Game/Card.cs
@@ -4,9 +4,7 @@ namespace W03_Prove_Hilo_Game.Game
 {
     public class Card
     {
-        public int valueOne = 0;
-        public int valueTwo = 0;
-        public int points = 0;
+        public int value = 0;
         public Card()
         {
         }
@@ -14,13 +12,7 @@ namespace W03_Prove_Hilo_Game.Game
         public void Draw()
         {
             Random random = new Random();
-            valueOne = random.Next(1,14);
-            valueTwo = random.Next(1,14);
-            if (valueTwo == valueOne )
-            {
-                valueTwo = random.Next(1,14);
- 
-            }
+            value = random.Next(1,14);
         }
     }
 }
--- W03_Prove_Hilo_Game/Game/Director.cs
+++ W03_Prove_Hilo_Game/Game/Director.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace W03_Prove_Hilo_Game.Game
 {
@@ -8,23 +9,46 @@ namespace W03_Prove_Hilo_Game.Game
         bool isPlaying = true;
         int score = 0;
         int totalScore = 300;
+        int playCount = 0;
+        string guess = "";
+        string playAgain = "";
 
         public Director()
         {
-
+            for (int i = 0; i < 2; i++)
+            {
+                Card card = new Card();
+                cards.Add(card);
+            }
         }
 
         public void StartGame()
         {
             while (isPlaying)
             {
+                // foreach (Card card in cards)
+                // {
+                //     Console.WriteLine(card.value);
+                // }
+                MakeCards();
                 DisplayCard();
-                GetInputs();
+                GetGuess();
+                DisplayCard();
                 DoUpdates();
-                DoOutputs();
+                DisplayScore();
+                PlayAgain();
+                Console.WriteLine();
+                
             }
         }
-
+        
+        public void MakeCards()
+        {
+            foreach (Card card in cards)
+                {
+                    card.Draw();
+                }
+        }
         public void DisplayCard()
         {
             if (!isPlaying)
@@ -32,7 +56,94 @@ namespace W03_Prove_Hilo_Game.Game
                 return;
             }
 
+            int cardOne = cards[0].value;
+            int cardTwo = cards[1].value;
+            // Console.WriteLine(cardOne);
+            // Console.WriteLine(cardTwo);
+            if (guess == "")
+            {
+                Console.WriteLine($"The card is: {cardOne}");
+            }
+            else if (guess == "h" || guess == "l")
+            {
+                Console.WriteLine($"Next card was: {cardTwo}");
+            }
             
+        }
+
+        public void GetGuess()
+        {
+            if (!isPlaying)
+            {
+                return;
+            }
+
+            Console.Write("Higher or lower? [h/l] ");
+            guess = Console.ReadLine();
+        }
+
+        public void DoUpdates()
+        {
+            if (!isPlaying)
+            {
+                return;
+            }
+            
+            int cardOne = cards[0].value;
+            int cardTwo = cards[1].value;
+            score = 0;
+
+            if (cardOne == cardTwo)
+            {
+                score = 0;
+                totalScore += score;
+            }
+            else if (cardOne > cardTwo && guess == "h") //next card was lower and user guessed higher
+            {
+                score = -75;
+                totalScore += score;
+            }
+            else if (cardOne < cardTwo && guess == "h") //next card was higher and user guess higher
+            {
+                score = 100;
+                totalScore += score;
+            }
+            else if (cardOne > cardTwo && guess == "l") //next card was lower and user guessed lower
+            {
+                score = 100;
+                totalScore += score;
+            }
+            else if (cardOne < cardTwo && guess == "l") //next card was higher and user guessed lower 
+            {
+                score = -75;
+                totalScore += score;
+            }
+
+            playCount +=1;
+            isPlaying = (totalScore > 0);
+        }
+
+        public void DisplayScore()
+        {
+            Console.WriteLine($"Your score is: {totalScore}");
+        }
+
+        public void PlayAgain()
+        {
+            if (!isPlaying)
+            {
+                return;
+            }
+
+            Console.Write("Play again? [y/n] ");
+            playAgain = Console.ReadLine();
+            isPlaying = (playAgain == "y");
+            guess = "";
+
+            if (playAgain == "n")
+            {
+                Console.WriteLine($"\nAmount of times played: {playCount}");
+            }
         }
     }
 }
